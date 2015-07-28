2089d699b3df1291d9ea7cedd3e2690ea9395b36
Elias Platek <elias.platek@gmail.com>
2015-07-28 22:12:55 +0200

    Updated socketio client endpoint address

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 ﻿window.onload = function () {
 	
 	var messages = [];
-	var socket = io.connect('http://localhost:5000');
+	var socket = io.connect('https://ancient-coast-2668.herokuapp.com/');
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
